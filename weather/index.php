79b3626ea322800af81630e2cdfb7333217b085c
Kimmo Tapala <kimitri@gmail.com>
2020-06-17 17:39:46 +0300

    Move CSS from link elements to a Link header just because.

--- weather/index.php
+++ weather/index.php
@@ -4,9 +4,17 @@ require_once('classes/WeatherNow.class.php');
 require_once('classes/WeatherToday.class.php');
 require_once('classes/WeatherDaily.class.php');
 
-header('Content-Type: text/html; charset=utf-8');
-$data = OpenWeatherMap::load();
+$css = array_map(function($url) {
+    return sprintf('<%s>; rel=stylesheet', $url);
+  }, 
+  array(
+    'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;&display=swap',
+    'css/styles.css'
+  ));
 
+header('Content-Type: text/html; charset=utf-8');
+header('Link: ' . implode(', ', $css));
+$data = OpenWeatherMap::load();
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -14,8 +22,6 @@ $data = OpenWeatherMap::load();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sää</title>
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
   <?php echo WeatherNow::render($data); ?>
